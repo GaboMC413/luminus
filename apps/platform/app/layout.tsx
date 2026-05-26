7b15156ec85347f8b860786d2c69f5993591bac7
Gabriel Medero <121062749+GaboMC413@users.noreply.github.com>
2026-05-26 17:16:37 -0300

    API KEY GOOGLE

--- apps/platform/app/layout.tsx
+++ apps/platform/app/layout.tsx
@@ -22,10 +22,12 @@ export default function RootLayout({
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-25..0" />
       </head>
       <body className={inter.className}>
-        <Script
-          src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&libraries=places`}
-          strategy="afterInteractive"
-        />
+        {process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY && (
+          <Script
+            src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&libraries=places`}
+            strategy="afterInteractive"
+          />
+        )}
         {children}
       </body>
     </html>
