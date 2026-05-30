dacf6111d56ad786c40ee6c21452069671400a1c
martinoneilferrari-design <martinoneilferrari@gmail.com>
2026-05-30 19:57:50 -0300

    Include Prisma client in platform server bundle

--- apps/platform/next.config.js
+++ apps/platform/next.config.js
@@ -1,4 +1,16 @@
+const path = require("path");
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    outputFileTracingRoot: path.join(__dirname, "../../"),
+    outputFileTracingIncludes: {
+      "/**": [
+        "./node_modules/.prisma/client/**/*",
+        "./node_modules/@prisma/client/**/*",
+      ],
+    },
+  },
+};
 
 module.exports = nextConfig
