506202de71515e9f0280ac5c4ec51ebce6fe791a
Gabriel Medero <121062749+GaboMC413@users.noreply.github.com>
2026-08-14 22:25:21 -0300

    style(marketing): set min-h-110vh on listing wrapper to push footer below the fold

--- apps/marketing/app/entrevistas/page.tsx
+++ apps/marketing/app/entrevistas/page.tsx
@@ -53,7 +53,7 @@ export default async function EntrevistasListingPage() {
   return (
     <main className="w-full min-h-screen bg-white flex flex-col justify-start items-start">
       <Navbar />
-      <div className="w-full pt-[64px]">
+      <div className="w-full pt-[64px] flex-1 flex flex-col min-h-[110vh]">
         <InterviewsSection events={events} isGrid={true} showTitle={false} />
       </div>
       <Footer />
